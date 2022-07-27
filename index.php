ff1a9065a7265a00fe68bfc872758982705bb197
Mihail KAlugin <apoc2015@yandex.ru>
2022-07-27 17:48:52 +0400

    initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header-page">
+        <div class="breadcrumb">
+            <ul class="breadcrumblist">
+                <li class="breadcrumblist__item"><a href=""></a></li>
+                <li class="breadcrumblist__item"><a href=""></a></li>
+            </ul>
+        </div>
+        <div class="page-heading">
+            <h2 class="heading"></h2>
+        </div>
+    </div>
+    <section class="price">
+        <div class="container">
+            <ul class="price__list">
+                <li class="list__item">
+                    <div class="card">
+                        <picture class="picture">
+                            <img src="" alt="" class="image">
+                        </picture>    
+                        <p class="card__short-descriprion"></p>   
+                        <div class="card__price"></div> 
+                        <div class="card__button-wrapper">
+                            <a class="card__button-buy" href=""></a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>        
+    </section>
+    <secton class="contacts">
+        <div class="container">
+            <div class="contacts__left">
+                <span class="contacts__title"></span>
+                <span class="contacts__address"></span>
+                <a class="contacts__phone" href="tel:+74994600781">+7 (499) 460-07-81</a>   
+                <div class="contacts__opening-hours">
+                    <ul class="hours__time-list">
+                        <li class="time-list__item"></li>
+                        <li class="time-list__item"></li>
+                        </ul> 
+                </div>
+            </div>
+            <div class="contacts__right">
+                <div class="contacts__map"></div>
+            </div>
+        </div>
+    </secton>
+</body>
+</html>
